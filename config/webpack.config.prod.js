24587156d18d8b37a0502a5cd7ed1082a8d449cd
Cody Welsh <codyw@protonmail.com>
2018-07-16 17:32:31 -0700

    feat(Webpack): Make prod webpack config

---
+++ config/webpack.config.prod.js
@@ -0,0 +1,55 @@
+// Development Webpack Configuration
+const path = require('path');
+const miniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const extractCSS = new miniCssExtractPlugin({
+  filename: '[name].css'
+});
+
+const HtmlWebpack = new HtmlWebpackPlugin({
+  title: 'Codekite Demo',
+  template: path.resolve(__dirname, '../dist/index.tmpl.html')
+});
+
+const config = {
+  mode: 'development',
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: '[name].bundle.js',
+    publicPath: './'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: miniCssExtractPlugin.loader
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              importLoaders: 1
+            }
+          }, {
+            loader: "postcss-loader"
+          }],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }],
+  },
+  plugins: [
+    extractCSS,
+    HtmlWebpack
+  ]
+}
+
+module.exports = config;
