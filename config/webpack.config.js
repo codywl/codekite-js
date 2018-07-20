605fba3d903985bff5c7f11ada309c6604a28ccd
Cody Welsh <codyw@protonmail.com>
2018-07-19 19:10:33 -0700

    fix(Webpack): Dev server CSS
    The dev server now correctly loads in CSS, and updates when changes are
    detected. Some lines from the production Webpack configuration have also
    been added, for cases where one does not wish to use
    `webpack-dev-server`.

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,25 +1,50 @@
 // Development Webpack Configuration
 const path = require('path');
+const miniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+// External CSS
+const extractCSS = new miniCssExtractPlugin({
+  filename: '[name].css'
+});
+
+// Injects script tags pointing to bundle
+const HtmlWebpack = new HtmlWebpackPlugin({
+  title: 'Codekite Demo (Dev)',
+  template: path.resolve(__dirname, '../src/html/index.tmpl.html')
+});
+
+// Cleans up generated bundles
+const CleanWebpack = new CleanWebpackPlugin(['../dist'], {allowExternal: true});
 
 const config = {
   mode: 'development',
   entry: './src/index.js',
   devtool: 'inline-source-map',
   output: {
-    path: path.resolve(__dirname, '../src'),
-    filename: '[main].bundle.js'
+    path: path.resolve(__dirname, '../dist'),
+    filename: '[name].bundle.js'
   },
   devServer: {
     contentBase: path.join(__dirname, '../dist'),
     host: "0.0.0.0",
     port: 9000
   },
+  plugins: [
+    extractCSS,
+    CleanWebpack,
+    HtmlWebpack
+  ],
   module: {
     rules: [
       {
         test: /\.css$/,
         exclude: /node_modules/,
         use: [
+          {
+            loader: miniCssExtractPlugin.loader
+          },
           {
             loader: "css-loader",
             options: {
