104458ea80fe8222907c5bed198a98529a3b213d
Cody Welsh <codyw@protonmail.com>
2018-08-13 19:19:28 -0700

    feat(Dev): Integrate Hot Reloading
    This commit hooks the `App` container into the hot reloading
    environment, so that both components and styles have hot reloading
    capability.

--- src/index.js
+++ src/index.js
@@ -7,12 +7,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './components/App';
-import './styles/styles.css';
 
 ReactDOM.render((
   <Router>
     <App/>
   </Router>
-),
-  document.getElementById('root')
+  ), document.getElementById('root')
 );
