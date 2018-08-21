063870d4a74c0bf58cd98b37227d19849b7a6e9d
Cody Welsh <codyw@protonmail.com>
2018-08-20 22:04:43 -0700

    styles(Nav): Move nav styles into its folder
    Breaks the styles off from the main stylesheet and into the `Navigation`
    component folder; as a consequence, the `postcss.config.js` file must be
    moved into the root directory so that it is detected from the context of
    component folders. I may use the root configuration exclusively in the
    future.

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,34 +1,38 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import styles from '../../styles/styles.css';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import styles from "./Navigation.styles.css";
 
 const routes = [
   {
-    path: '/home',
-    title: 'Home'
+    path: "/home",
+    title: "Home"
   },
   {
-    path: '/about',
-    title: 'About'
+    path: "/about",
+    title: "About"
   },
   {
-    path: '/users',
-    title: 'Users'
+    path: "/users",
+    title: "Users"
   }
 ];
 
 const Nav = () => {
   return (
     <div className={styles.nav}>
-      {routes.map((route, idx) => (
-        <NavLink
-          key={idx}
-          to={route.path}
-          className={styles.nav__link}
-          activeClassName={styles['nav__link--active']}>
-          {route.title}
-        </NavLink>
-      ))}
+      <NavLink to={routes[0].path} className={styles["nav__icon"]} />
+      <div className={styles["nav__linkslist"]}>
+        {routes.map((route, idx) => (
+          <NavLink
+            key={idx}
+            to={route.path}
+            className={styles.nav__link}
+            activeClassName={styles["nav__link--active"]}
+          >
+            {route.title}
+          </NavLink>
+        ))}
+      </div>
     </div>
   );
 };
--- src/styles/postcss.config.js
+++ src/styles/postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   plugins: {
-    'postcss-initial': {},
-    'cssnano': {},
-    'autoprefixer': {}
+    "postcss-initial": {},
+    cssnano: {},
+    autoprefixer: {}
   }
 };
