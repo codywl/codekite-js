53fb9bd745f1c117392407ca9603966060194167
Cody Welsh <codyw@protonmail.com>
2018-09-01 01:07:54 -0700

    fix(Tests): Convert AVA test to Jest
    Creates a similar test file to the AVA-centric one, but with Jest
    instead.

--- src/components/navigation/Navigation.test.js
+++ src/components/navigation/Navigation.test.js
@@ -4,9 +4,12 @@
 import React from "react";
 import renderer from "react-test-renderer";
 import { BrowserRouter as Router } from "react-router-dom";
+import { configure, mount } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
 import NavigationContainer from "./NavigationContainer";
 
+configure({ adapter: new Adapter() });
 const NavComp = () => {
   return (
     <Router>
@@ -20,4 +23,10 @@ test("renders without errors", () => {
   let tree = component.toJSON();
 
   expect(tree).toMatchSnapshot();
+});
+
+test("creates links from a routes array", () => {
+  const navcomp = mount(<NavComp />);
+  expect(navcomp).toHaveLength(1);
+  expect(navcomp.find(`a`)).toHaveLength(4);
 });
