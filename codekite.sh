9d039c09beb5b5450f73d1cf14d9f842bbb0b9a3
Cody Welsh <codyw@protonmail.com>
2018-11-16 16:28:27 -0800

    feat(Cfg): Add creation shell script
    This commit deletes `ngrok` from the repository, and instead downloads
    it from the user's machine after all other steps in the provided shell
    script succeed.

---
+++ codekite.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Author: Cody Welsh <c o d y w (at) protonmail (dot) com>
+
+# ngrok download URL
+NGROK_URL='https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip'
+
+# Check for folder existence
+echo "1 - Checking folder existence.."
+
+if [ -e $1 ]; then
+  echo "The directory $1 exists. Exiting.."
+  exit
+fi
+
+# Copy directory structure to specified location under the name
+echo "2 - Creating new project.."
+cp $(dirname $(readlink -f $0)) ./$1 -r
+
+
+# cd to dir
+echo "3 - Switching to project directory.."
+cd ./$1
+
+# cp . -rf $project
+# Download latest `ngrok` to the folder
+echo "4 - Downloading 'ngrok'.."
+curl $NGROK_URL > ngrok
+
+# Vanity message
+echo "Done. To start, run 'npm start' or 'docker build . && docker run . '."
